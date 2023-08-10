3d50d26d011dc5d507d1b5e9adf7aec1bd1d56a9
Coderovshik <peter.04007@gmail.com>
2023-08-10 15:56:21 +0300

    implement Formik for start page

--- src/routes/Start.tsx
+++ src/routes/Start.tsx
@@ -1,23 +1,18 @@
-import { useState } from 'react';
-import { Form, redirect } from 'react-router-dom';
+import { useRef } from 'react';
+import { Form, redirect, useSubmit } from 'react-router-dom';
+import { Formik } from 'formik';
 import TextField from '../components/textField';
+import PhoneField from '../components/PhoneField';
+import { startSchema } from '../utils/schemas';
 import User from '../storage';
 
 // styles
 import styles from '../styles/start.module.css';
 
-const textFields = [
-    {
-        label: "Номер телефона",
-        placeholder: "+7 999 999-99-99",
-        name: "phoneNumber",
-    },
-    {
-        label: "Email",
-        placeholder: "tim.jennings@example.com",
-        name: "email",
-    },
-];
+const initialValues = {
+    tel: '',
+    email: ''
+};
 
 const contacts = [
     {
@@ -34,24 +29,27 @@ const contacts = [
     },
 ];
 
-export async function action({ request } : { request: Request }) {
+export async function action({ request }: { request: Request }) {
     const formData = await request.formData();
     const updates = Object.fromEntries(formData);
+    User.step = 1;
     User.write(updates);
     return redirect("/form");
 }
 
 export default function Start() {
-    const [fields, setFields] = useState(Array(textFields.length).fill(""));
+    //const [fields, setFields] = useState(Array(textFields.length).fill(""));
+    const submit = useSubmit();
+    const formRef = useRef<HTMLFormElement>(null);
 
-    return(
+    return (
         <div className={styles.start}>
             <div className={styles.header}>
                 <span>ПУ</span>
                 <div>
                     <h1>Петр Удинцев</h1>
                     <p>
-                        {contacts.map((contact) => 
+                        {contacts.map((contact) =>
                             <a
                                 href={contact.link}
                                 target='_blank'
@@ -61,24 +59,35 @@ export default function Start() {
                 </div>
             </div>
             <hr />
-            <Form method='post'>
-                {textFields.map((field, index) => 
-                    <TextField 
-                        {...field} 
-                        defaultValue={fields[index]}
-                        onChange={(event) => {
-                            let copy = fields.slice();
-                            copy[index] = event.target.value;
-                            setFields(copy);
-                        }}
-                    />
-                )}
-                <button 
-                    id='button-start' 
-                    type='submit'
-                    onClick={() => User.step = 1}
-                >Начать</button>
-            </Form>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={startSchema}
+                onSubmit={(values, { setSubmitting }) => {
+                    setSubmitting(false);
+                    submit(formRef.current);
+                }}
+            >
+                {formik =>
+                    <Form method='post' ref={formRef} onSubmit={formik.handleSubmit}>
+                        <PhoneField
+                            label='Номер телефона'
+                            name='tel'
+                            placeholder='+7 999 999-99-99'
+                        />
+                        <TextField
+                            label='Email'
+                            name='email'
+                            placeholder='tim.jennings@example.com'
+                            type='email'
+                        />
+                        <button
+                            id='button-start'
+                            type='submit'
+                            disabled={formik.isSubmitting}
+                        >Начать</button>
+                    </Form>
+                }
+            </Formik>
         </div>
     );
 }
