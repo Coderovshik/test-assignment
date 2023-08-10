6272d7763d5f96b9dcb8296547111a1525267d80
Coderovshik <peter.04007@gmail.com>
2023-08-10 15:44:04 +0300

    corrected start page schema

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,6 +1,6 @@
-import { object, string } from 'yup';
+import * as Yup from 'yup';
 
-export const startSchema = object({
-    phoneNumber: string().matches(/^\+7 \d{3} \d{3}-\d{2}-\d{2}/),
-    email: string().email(),
+export const startSchema = Yup.object({
+    tel: Yup.string().matches(/^(\+7|8) \(\d{3}\) \d{3}-\d{2}-\d{2}$/, { message: 'Invalid telephone number' }).required('Required'),
+    email: Yup.string().email('Invalid email adress').required('Required')
 });
